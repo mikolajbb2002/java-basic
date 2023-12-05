25b8b6ccc82fbd11b2820b68fa98bba03d0a4f84
Miki <mikolajbb2002@users.noreply.github.com>
2023-12-05 13:03:09 +0100

    Basic stuff

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+#   first_num = input("Na ktorym miejscu znajduje sie 1  ")
+#finder = (first_num.find("1"))
+#miejsce = (int(finder) + 1)
+#if miejsce==0:
+#    print("nie ma 1 w tej liczbie ")
+#else:
+#    print("Jedynka znajduje sie na:" + str(miejsce))
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+miasto = input("Podaj miasto w ktorym dorastales:")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+imie= input("Podaj imie zwierzaka")
+print("Nazwa twojej kapeli to: " + miasto + imie)
+print(len(miasto))
