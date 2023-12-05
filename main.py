8b9779010ac2e47e5a377e74568c2bbfa90abff5
Miki <mikolajbb2002@users.noreply.github.com>
2023-12-05 13:39:36 +0100

    Java course day 3

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-#   first_num = input("Na ktorym miejscu znajduje sie 1  ")
-#finder = (first_num.find("1"))
-#miejsce = (int(finder) + 1)
-#if miejsce==0:
-#    print("nie ma 1 w tej liczbie ")
-#else:
-#    print("Jedynka znajduje sie na:" + str(miejsce))
+print("Welcome to the game! Your goal is to find a treasure")
+first_choice=input("You're at a cross road. Where do you want to go? Type 'left' or 'right'\n")
+if first_choice=="left":
+    second_choice = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n")
+    if second_choice=="wait":
+        third_choice= input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
+        if third_choice=="yellow":
+            print("You found the treasure! You Win!")
+        elif third_choice=="red":
+            print("It's a room full of fire. Game Over.")
+        elif third_choice=="blue":
+            print("You enter a room of beasts. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
 
-miasto = input("Podaj miasto w ktorym dorastales:")
-
-imie= input("Podaj imie zwierzaka")
-print("Nazwa twojej kapeli to: " + miasto + imie)
-print(len(miasto))
+else:
+    print("You fell into a hole. Game Over.")
